fix mash gradient missing by calling loadParam func, now will replace params directly

mash_2dgs/Model/mash_gs.py
@@ -21,11 +21,11 @@ from ma_sh.Method.pcd import getPointCloud, downSample
 from mash_2dgs.Method.rotation import matrix_to_quaternion
 
 class MashGS(object):
-    def __init__(self, sh_degree: int, anchor_num: int=100,
+    def __init__(self, sh_degree: int, anchor_num: int=400,
         mask_degree_max: int = 0,
-        sh_degree_max: int = 1,
+        sh_degree_max: int = 0,
         sample_phi_num: int = 10,
-        sample_theta_num: int = 4,
+        sample_theta_num: int = 40,
         use_inv: bool = True,
         idx_dtype=torch.int64,
         dtype=torch.float32,
@@ -46,11 +46,12 @@ class MashGS(object):
         self.mash = SimpleMash(anchor_num, mask_degree_max, sh_degree_max, sample_phi_num, sample_theta_num, use_inv, idx_dtype, dtype, device)
         self.mash.setGradState(True)
 
-        self.surface_dist = 0.05
+        self.surface_dist = 0.5
+        self.split_dist_weight = 2.0
 
-        self.split_thresh = 0.5
-        self.clone_thresh = 0.5
-        self.prune_thresh = 0.5
+        self.split_thresh = 0.2
+        self.clone_thresh = 0.2
+        self.prune_thresh = 0.9
 
         self.split_pts_num = 0
         self.clone_pts_num = 0
@@ -374,12 +375,10 @@ class MashGS(object):
 
         self.mash.anchor_num = optimizable_tensors["positions"].shape[0]
         self.mash.reset()
-        self.mash.loadParams(
-            optimizable_tensors["mask_params"],
-            optimizable_tensors["sh_params"],
-            optimizable_tensors["rotate_vectors"],
-            optimizable_tensors["positions"]
-        )
+        self.mash.mask_params = optimizable_tensors["mask_params"]
+        self.mash.sh_params = optimizable_tensors["sh_params"]
+        self.mash.rotate_vectors = optimizable_tensors["rotate_vectors"]
+        self.mash.positions = optimizable_tensors["positions"]
         self.updateGSParams()
 
         self._features_dc = optimizable_tensors["f_dc"]
@@ -425,12 +424,10 @@ class MashGS(object):
         optimizable_tensors = self.cat_tensors_to_optimizer(d)
         self.mash.anchor_num = optimizable_tensors["positions"].shape[0]
         self.mash.reset()
-        self.mash.loadParams(
-            optimizable_tensors["mask_params"],
-            optimizable_tensors["sh_params"],
-            optimizable_tensors["rotate_vectors"],
-            optimizable_tensors["positions"]
-        )
+        self.mash.mask_params = optimizable_tensors["mask_params"]
+        self.mash.sh_params = optimizable_tensors["sh_params"]
+        self.mash.rotate_vectors = optimizable_tensors["rotate_vectors"]
+        self.mash.positions = optimizable_tensors["positions"]
         self.updateGSParams()
 
         self._features_dc = optimizable_tensors["f_dc"]
@@ -481,6 +478,8 @@ class MashGS(object):
             samples = 0.0
         else:
             samples = torch.vstack(anchor_delta_xyzs).reshape(-1, 3)
+
+        samples = samples * self.split_dist_weight
 
         new_mask_params = self.mash.mask_params[selected_anchor_mask].repeat(N,1) / (0.8*N)
         new_sh_params = self.mash.sh_params[selected_anchor_mask].repeat(N,1)

train.py
@@ -32,14 +32,14 @@ from mash_2dgs.Method.render import renderMashGS
 import mash_cpp
 
 USE_TENSORBOARD = True
-GSMODEL = GaussianModel
+GSMODEL = MashGS
 render_freq = -1
 
 def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoint_iterations, checkpoint):
     save_graph = False
-    fit_loss_weight = 10.0
-    coverage_loss_weight = 10.0
-    boundary_connect_loss_weight = 10.0
+    fit_loss_weight = 1e1
+    coverage_loss_weight = 1e1
+    boundary_connect_loss_weight = 1e1
 
     first_iter = 0
     tb_writer = prepare_output_and_logger(dataset)
@@ -115,10 +115,13 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         normal_loss = lambda_normal * (normal_error).mean()
         dist_loss = lambda_dist * (rend_dist).mean()
 
-        if iteration > opt.densify_until_iter:
-            opacity_loss = 1e-3 * torch.nn.MSELoss()(gaussians.get_opacity, torch.ones_like(gaussians._opacity))
+        if False and isinstance(gaussians, GaussianModel):
+            if iteration > opt.densify_until_iter:
+                opacity_loss = 1e-3 * torch.nn.MSELoss()(gaussians.get_opacity, torch.ones_like(gaussians._opacity))
+            else:
+                opacity_loss = 1e-3 * torch.nn.MSELoss()(gaussians.get_opacity, torch.zeros_like(gaussians._opacity))
         else:
-            opacity_loss = 1e-3 * torch.nn.MSELoss()(gaussians.get_opacity, torch.zeros_like(gaussians._opacity))
+            opacity_loss = torch.tensor(0.0).type(dist_loss.dtype).to(dist_loss.device)
 
         fit_loss = torch.tensor(0.0).type(dist_loss.dtype).to(dist_loss.device)
         coverage_loss = torch.tensor(0.0).type(dist_loss.dtype).to(dist_loss.device)
@@ -336,7 +339,7 @@ if __name__ == "__main__":
     parser.add_argument('--ip', type=str, default="127.0.0.1")
     parser.add_argument('--port', type=int, default=6009)
     parser.add_argument('--detect_anomaly', action='store_true', default=False)
-    parser.add_argument("--test_iterations", nargs="+", type=int, default=list(range(0, 30000, 1000)))
+    parser.add_argument("--test_iterations", nargs="+", type=int, default=list(range(0, 30000, 100)))
     parser.add_argument("--save_iterations", nargs="+", type=int, default=list(range(0, 30000, 1000)))
     parser.add_argument("--quiet", action="store_true")
     parser.add_argument("--checkpoint_iterations", nargs="+", type=int, default=[])